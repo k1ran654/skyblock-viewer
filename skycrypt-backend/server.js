af8eb979a357179328a653fd6af540be4823bd7e
Jirka <jirkakolenaty@gmail.comgit commit -m initial>
2025-06-21 10:02:42 +0200

    port update

--- skycrypt-backend/server.js
+++ skycrypt-backend/server.js
@@ -53,6 +53,6 @@ app.get("/api/skyblock/:uuid", async (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
 });
